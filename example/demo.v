46b3fbf4ecc102c3ee26aaecc1b137aaba6b3aa7
Maxime Daniel <root@maxux.net>
2023-09-01 14:46:37 +0200

    vssh: add constructor test

--- example/demo.v
+++ example/demo.v
@@ -1,3 +1,6 @@
 import maxux.vssh
 
 vssh.test()
+
+x := vssh.new(.agent, "", "hello", "")!
+y := vssh.new(.password, "", "hello", "world")!
--- vssh.v
+++ vssh.v
@@ -7,6 +7,33 @@ module vssh
 #flag -lssh2
 #flag -DVSSH_NO_MAIN
 
+pub enum Authentication {
+	agent
+	password
+}
+
+struct SSH2 {
+	ssh &C.ssh_t
+}
+
+pub fn new(method Authentication, host string, user string, password string) !SSH2 {
+	ssh := SSH2{}
+
+	if method == .agent {
+		println("SSH AGENT")
+	}
+
+	if method == .password {
+		println("BY PASSWORD")
+	}
+
+	println(method)
+	println(user)
+	println(password)
+
+	return ssh
+}
+
 pub fn test() {
 	println("Hello world")
 }
