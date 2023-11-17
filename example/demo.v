6aadfeeab289a6ac43a5ca7265e89cd2b3078176
Maxime Daniel <root@maxux.net>
2023-11-17 17:42:16 +0100

    module: debug command streaming

--- example/demo.v
+++ example/demo.v
@@ -4,6 +4,7 @@ x := vssh.new("10.241.0.240", 22)!
 println(x.fingerprint())
 x.authenticate(.agent, "root", "")!
 x.execute("uname -a")!
+x.stream("dmidecode")!
 x.disconnect()
 
 y := vssh.new("10.241.0.230", 22)!
